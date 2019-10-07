2702f408fbd071e36ce6a3b02dcb25943c00a8b1
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-10-08 05:31:06 +0700

    membuat property baru

--- produk.php
+++ produk.php
@@ -7,9 +7,19 @@
 
 class Produk {
 
-
+	public $judul = "judul",
+		   $penulis = "penulis",
+		   $penerbit ="penerbit",
+		   $harga = 0;
 }
 
 
 $produk1 = new Produk();
+$produk1->judul ="Naruto";
+var_dump($produk1);
+
+
 $produk2 = new Produk();
+$produk2->judul ="Uncharted";
+$produk2->tambahProperty = "hahahah";
+var_dump($produk2);
