0d898555f5f876d4d7e7b5fdfa880e7db6c42d89
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-10-09 23:09:03 +0700

    mencoba melakukan atau memahami autoloading

--- interface.php
+++ interface.php
@@ -16,9 +16,7 @@ interface InfoProduk{
 			$harga,
 			$diskon = 0;
 	
-			
-
-	public function __construct($judul="judul",$penulis="penulis",$penerbit="penerbit",$harga=0) {
+public function __construct($judul="judul",$penulis="penulis",$penerbit="penerbit",$harga=0) {
 				$this->judul = $judul;
 				$this->penulis = $penulis;
 				$this->penerbit = $penerbit;
