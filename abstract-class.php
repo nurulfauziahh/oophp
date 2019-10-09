d9f805da5b4d5d099ec079869781af2249422688
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-10-09 23:04:13 +0700

    memodify file abstract-class

--- abstract-class.php
+++ abstract-class.php
@@ -78,3 +78,64 @@ abstract class Produk{//sebuah class
 		return $str;
 	}
 }	
+
+class Komik extends Produk{
+		public $jmlHalaman;
+		public function __construct($judul="judul",$penulis="penulis",$penerbit="penerbit",$harga=0,$jmlHalaman= 0){
+			parent::__construct($judul,$penulis,$penerbit,$harga);
+			$this->jmlHalaman = $jmlHalaman;
+
+		}
+
+	public function getInfoProduk(){
+			$str = "Komik : " . $this->getInfo() . " - {$this->jmlHalaman} Halaman.";
+			return $str;
+		}
+	}
+
+ 
+
+	class Game extends Produk{
+		public $waktuMain;
+		public function __construct($judul="judul",$penulis="penulis",$penerbit="penerbit",$harga=0,$waktuMain= 0){
+			parent::__construct($judul,$penulis,$penerbit,$harga);
+			$this->waktuMain = $waktuMain;
+		}
+
+
+		public function getInfoProduk(){
+			$str = "Game : " . $this->getInfo() . "  ~ {$this->waktuMain} Jam.";
+			return $str;
+	}
+
+}
+	class CetakInfoProduk{
+		public $daftarProduk = array();
+
+		public function tambahProduk(Produk $produk){
+			$this->daftarProduk[]=$produk;
+		}
+
+		public function cetak(){
+			$str = "DAFTAR PRODUK : <br>";
+			
+	foreach($this->daftarProduk as $p ){
+		$str .= "-{$p->getInfoProduk()} <br>";
+}
+
+	return $str;
+	}
+	}
+
+
+	$produk1 = new Komik("Naruto","Masashi Kishimoto","Shonen Jump",30000,100);
+	$produk2 = new Game("Unceharted","Neil Druckmann","Sony Computer",250000,50);
+	
+	
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
+
+	
+?>
